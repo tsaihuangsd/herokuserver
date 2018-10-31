ff898c700daac393ab20069344334f47141585ae
Tsai Huang <tsaihuangsd@gmail.com>
2018-10-31 10:05:56 -0700

    Add comments on port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 const server = require('./api/server.js');
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 8000; //either use the dynamic port provided by heroku, or use a port number defined by user on local host during development
 const instance = server.listen(port, () =>
 	console.log(`\n=== Server running on port: ${instance.address().port} ===\n`)
 );
